2589599879ef6c6842d4c68d8edb2d45ddd42a1a
KhanhDung <92654803+KhanhDung2702@users.noreply.github.com>
2022-08-10 15:24:00 +0700

    js

--- Final/js/main.js
+++ Final/js/main.js
@@ -1,17 +1,41 @@
-var mode = document.getElementById("mode");
-mode.addEventListener('click', function(){
-    document.body.classList.toggle("light-theme");
-})
-
-// audio
 window.addEventListener("load", ()=>{
-    const sounds = document.querySelectorAll("sound");
-    const board =document.querySelectorAll(".board div")
+    // dark light mode
+    const mode = document.getElementById("mode");
+    mode.addEventListener('click', function(){
+        document.body.classList.toggle("light-theme");
+    })
 
-    board.forEach((box, index) =>{
-        box.addEventListener("click", function(){
-            sounds[index].curentTime=0;
-            sounds[index].play();
+    // audio
+    const vol__bar = document.querySelectorAll(".vol__bar"); 
+    const play = document.querySelectorAll(".play div");
+    const sounds = document.querySelectorAll(".sound"); 
+    const item__up = document.querySelectorAll(".item__up"); 
+
+    play.forEach((play__item, index) => {
+        play__item.addEventListener("click", () => {
+            vol__bar[index].classList.toggle("vol__open");
+            if(play__item.dataset.playing==="false"){
+                play__item.dataset.playing="true";
+                sounds[index].play();
+            }
+            else{
+                play__item.dataset.playing="false";
+                sounds[index].pause();
+            }
         });
-    });
+     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
