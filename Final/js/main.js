8d63f3eb8cf598882f0d1ce5f5d3118e91f7b052
KhanhDung <92654803+KhanhDung2702@users.noreply.github.com>
2022-08-15 16:07:45 +0700

    vol

--- Final/js/main.js
+++ Final/js/main.js
@@ -9,17 +9,26 @@ window.addEventListener("load", ()=>{
     const vol__bar = document.querySelectorAll(".vol__bar"); 
     const sounds = document.querySelectorAll(".sound"); 
     
-    Array.from(document.getElementsByClassName("play__img")).forEach((element , index)=>{
-        element.addEventListener('click', (e)=>{
+    Array.from(document.getElementsByClassName("play__img")).forEach((element, index)=>{
+        element.addEventListener('click', ()=>{
             vol__bar[index].classList.toggle("vol__open");
+            
             if(element.dataset.playing==="false"){
                 element.dataset.playing="true";
                 sounds[index].play();
+                
             }
             else{
                 element.dataset.playing="false";
                 sounds[index].pause();
             }
+        })
+    })
+    // volume
+    Array.from(document.getElementsByClassName("vol__bar")).forEach((element, index)=>{
+        element.addEventListener('change', ()=>{
+            const vol__value=element.value;
+            sounds[index].volume = vol__value/100;
         })
     })
 });
